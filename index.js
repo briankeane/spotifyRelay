84540f650184b18448fc2692e41b6f86b947f717
briankeane <lonesomewhistle@gmail.com>
2017-07-31 11:17:58 -0500

    fix directory structure.  install mocha, chai,

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
-const router = require('./api/routes').default;
+const router = require('./server/api/routes').default;
 
 
 // app setup
---
+++ server/api/user/user.api.spec.js
@@ -0,0 +1,7 @@
+var app = require('../../../index');
+
+describe('test', function () {
+  it ('tests', function () {
+    console.log('running tests');
+  });
+});
